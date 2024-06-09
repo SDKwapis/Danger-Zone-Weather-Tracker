3d9145b142d4300ae5ce290e07577e728b4cf939
Stephan Kwpais <stephankwpais@Stephans-Mac-mini.local>
2024-06-09 09:34:42 -0400

    Added more css and JS

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const cityInput = document.querySelector('#srchbttn');
 const cityName = document.querySelector('#searchCity');
+const cityButton = document.createElement('button');
 
 const cityList = [];
 
@@ -8,4 +9,16 @@ cityInput.addEventListener('click', function(event) {
     const cityValue = cityName.value;
     cityList.push(cityValue);
     localStorage.setItem('City', JSON.stringify(cityList));
+    document.getElementById("searchCity").reset();
 });
+
+
+function renderCities() {
+    for (let i = 0; i< cityList.length; i++) {
+        const cities = cityList[i];
+
+        const button = document.createElement('button');
+        button.textContent = cities;
+
+    }
+}
