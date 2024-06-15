6e099be5e68d68160bdf4684ea3ead2012c1a7cf
Stephan Kwpais <stephankwpais@Stephans-Mac-mini.local>
2024-06-15 19:06:47 -0400

    Added JS for weather data info population

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const cityInput = document.querySelector('#srchbttn');
 const cityName = document.querySelector('#searchCity');
 
-
-
 const cityList = [];
+
+function getCurrentFormattedDate() {
+    const currentDate = new Date();
+    const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+    const day = currentDate.getDate().toString().padStart(2, '0');
+    const year = currentDate.getFullYear();
+    const formattedDate = `${month}/${day}/${year}`;
+  
+    return formattedDate;
+  }
+  const currentDateFormatted = getCurrentFormattedDate();
 
 // pulls the searched cities from local storage and displays them in the searched cities list
 document.addEventListener('DOMContentLoaded', function(event) {
@@ -19,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
     }
 });
 
-// adds new cities to the searched cities list and stores them in local storage
+// Search for a city button function - adds new cities to the searched cities list and stores them in local storage
 cityInput.addEventListener('click', function(event) {
     event.preventDefault();
     const cityValue = cityName.value;
@@ -34,10 +43,50 @@ cityInput.addEventListener('click', function(event) {
     bttn.innerHTML = cityValue;
     cityButtonList.appendChild(bttn);
     cityName.value = "";
-    }
-});
+    const dataExist = document.getElementById('weatherDisplay');
+    dataExist.innerHTML = '';
+    const apiKey = 'f63ca6bf62c5c8783f6c0a4d9033f7ec';
+    const geoCode = `http://api.openweathermap.org/geo/1.0/direct?q=${cityValue}&limit=1&appid=${apiKey}`;
+    
+    fetch(geoCode)
+    .then(function(res) {
+        console.log(res);
+        return res.json();
+    })
+    .then(function(data) {
+        const [{lat, lon}] = data;
+        const apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
-// API call
+        fetch(apiUrl)
+        .then(function(res) {
+            console.log(res);
+            return res.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            const {current: {temp, humidity, wind_speed}, } = data;
+            console.log(temp)
+
+            const cityTitle = document.getElementById('weatherDisplay');
+            const cityDisplay = document.createElement('h2');
+            const cityTemp = document.createElement('p');
+            const cityWind = document.createElement('p');
+            const cityHum = document.createElement('p');
+            cityDisplay.innerHTML = `${cityValue} ${currentDateFormatted}`;
+            cityTemp.innerHTML = `Temp: ${Math.round((temp - 273.15)*1.8+32)}\u00B0C`;
+            cityWind.innerHTML = `Wind: ${wind_speed} mph`;
+            cityHum.innerHTML = `Humidity: ${humidity}%`;
+            cityTitle.appendChild(cityDisplay);
+            cityTitle.appendChild(cityTemp);
+            cityTitle.appendChild(cityWind);
+            cityTitle.appendChild(cityHum);
+
+        });
+    });
+}});
+
+
+// API call - makes city list buttons clickable
 const preCity = document.getElementById('cityButtons');
 const apiKey = 'f63ca6bf62c5c8783f6c0a4d9033f7ec';
 const cityNameInput = cityName.value;
@@ -45,9 +94,11 @@ const cityNameInput = cityName.value;
 // const geoCode = `http://api.openweathermap.org/geo/1.0/direct?q=${cityValue}&limit=1&appid=${apiKey}`;
 
 
-preCity.addEventListener('click', function(event) {
-    event.preventDefault();
-    const cityValue = preCity.innerText;
+preCity.addEventListener('click', function(preCity) {
+    // event.preventDefault();
+    const dataExist = document.getElementById('weatherDisplay');
+    dataExist.innerHTML = '';
+    const cityValue = preCity.target.textContent;
     console.log('City:', cityValue);
     const apiKey = 'f63ca6bf62c5c8783f6c0a4d9033f7ec';
     const geoCode = `http://api.openweathermap.org/geo/1.0/direct?q=${cityValue}&limit=1&appid=${apiKey}`;
@@ -68,15 +119,15 @@ preCity.addEventListener('click', function(event) {
         })
         .then(function(data) {
             console.log(data);
-            const {current: {temp, humidity, dt, wind_speed}} = data;
+            const {current: {temp, humidity, dt, wind_speed}, } = data;
             console.log(temp)
-            
+
             const cityTitle = document.getElementById('weatherDisplay');
             const cityDisplay = document.createElement('h2');
             const cityTemp = document.createElement('p');
             const cityWind = document.createElement('p');
             const cityHum = document.createElement('p');
-            cityDisplay.innerHTML = `${cityValue} (${dt})`;
+            cityDisplay.innerHTML = `${cityValue} ${currentDateFormatted}`;
             cityTemp.innerHTML = `Temp: ${Math.round((temp - 273.15)*1.8+32)}\u00B0C`;
             cityWind.innerHTML = `Wind: ${wind_speed} mph`;
             cityHum.innerHTML = `Humidity: ${humidity}%`;
@@ -85,12 +136,6 @@ preCity.addEventListener('click', function(event) {
             cityTitle.appendChild(cityWind);
             cityTitle.appendChild(cityHum);
 
-            
         });
     });
 });
-
-   
-
-
-
